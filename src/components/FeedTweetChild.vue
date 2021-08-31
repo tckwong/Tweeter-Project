7949228486a249a160f89396e24ad55d232e8cad
Trevor <wtrevor242@gmail.com>
2021-08-31 17:02:20 -0600

    Added new commentTweetChild, and comment APIs for GET, POST

--- src/components/FeedTweetChild.vue
+++ src/components/FeedTweetChild.vue
@@ -1,76 +1,104 @@
 <template>
-    <div>
-    <v-card
-    class="mx-auto"
-    color="#26c6da"
-    dark
-    max-width="40vw"
-  >
-    <v-card-title>
-      <v-icon
-        large
-        left
-      >
-        TWEETER
-      </v-icon>
-      <span class="text-h6 font-weight-light">{{ createdAt }} <v-btn @click="deleteTweet" color="red" elevation="2" Small>✕</v-btn></span>
-    </v-card-title>
-   
-    <v-card-text class="text-h5 font-weight-bold">
-        {{ content }}
-    </v-card-text>
-
-    <v-card-actions>
-      <v-list-item class="grow">
-        <v-list-item-avatar color="grey darken-3">
-          <v-img
-            class="elevation-6"
-            alt=""
-            src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"
-          ></v-img>
-        </v-list-item-avatar>
-
-        <v-list-item-content>
-          <v-list-item-title><router-link :to="{ name: 'OtherProfilesView', params: { user: username }}">{{ username }}</router-link></v-list-item-title>
-        </v-list-item-content>
-        
-        <v-row
-          align="center"
-          justify="end"
+    <div class="tweetWrapper">
+        <v-card
+        class="mx-auto"
+        color="#6aaaff"
+        dark
+        max-width="50vw"
+        min-height="30vh"
         >
-        <!-- Embed a button around v-icon for click-->
-        <img id="commentIcon" @click="showModal" :src="require('@/assets/speech-bubble.png')"/>
-        <div class="modal" v-if="showModal">
-            <h1>Lorem Ipsum</h1>
-            <button class="button" @click="showModal">Close Modal</button>
-        </div>
-        <v-spacer></v-spacer>
+        <v-card-title>
+            <img id="logo" src="@/assets/logo.png"/>
+            <span class="text-h6 font-weight-light">{{ createdAt }} <v-btn @click="deleteTweet" color="red" elevation="2" Small>✕</v-btn></span>
+        </v-card-title>
+    
+        <v-card-text class="text-h5 font-weight-bold">
+            {{ content }}
+        </v-card-text>
 
-          <div :class="{ likedDisplay: toggleLike }"> 
-                <v-icon @click="checkTweetLiked" class="mr-1">
-                mdi-thumb-up
-                 </v-icon>
-          </div>
+        <v-card-actions>
+        <v-list-item class="grow">
+            <v-list-item-avatar color="grey darken-3">
+            <v-img
+                class="elevation-6"
+                alt=""
+                src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"
+            ></v-img>
+            </v-list-item-avatar>
+
+            <v-list-item-content>
+            <v-list-item-title><router-link class="routerLink" :to="{ name: 'OtherProfilesView', params: { user: username }}">{{ username }}</router-link></v-list-item-title>
+            </v-list-item-content>
+            
+            <v-row
+            align="center"
+            justify="end"
+            >
+            <!-- Embed a button around v-icon for click-->
         
-          <span class="subheading mr-2">{{ tweetLikeCounter }}</span>
-          <span class="mr-1">·</span>
+            <v-text-field
+                class="formInput mr-8"            
+                v-model="tweetComment"
+                label="Comment"
+                placeholder="Comment"
+                solo
+            ></v-text-field>
+            <div class="mr-3">
+                <img id="commentIcon" @click="createComment" :src="require('@/assets/speech-bubble.png')"/>
+            </div>
+     
+            <div :class="{ likedDisplay: toggleLike }"> 
+                    <v-icon @click="checkTweetLiked" class="mr-1">
+                    mdi-thumb-up
+                    </v-icon>
+            </div>
+            
+            <span class="subheading mr-2">{{ tweetLikeCounter }}</span>
+            <span class="mr-1">·</span>
+        
+            <button @click="retrieveUserId" class="myButton">Follow</button>
+            </v-row>
        
-              <button @click="retrieveUserId" class="myButton">Follow</button>
-        </v-row>
-      </v-list-item>
-    </v-card-actions>
-  </v-card>
-   <!-- <transition name="fade" appear>
-            <div class="modal-overlay" v-if="showModal"></div>
-        </transition> -->
+        </v-list-item>
+       
+        </v-card-actions>
+        <v-card-actions>
+              <v-expansion-panels>
+                <v-expansion-panel
+                v-for="(item,i) in 1"
+                :key="i"
+                >
+                <v-expansion-panel-header @click="retrieveTweetComments">
+
+                </v-expansion-panel-header>
+                <v-expansion-panel-content>
+                   <TweetComments v-for="comment in tweetCommentInfo"
+                    :key="comment.commentId"
+                    :commentId="comment.commentId"
+                    :tweetId="comment.tweetId"
+                    :username="comment.username"
+                    :content="comment.content"
+                    :createdAt="comment.createdAt"/>
+                </v-expansion-panel-content>
+                </v-expansion-panel>
+            </v-expansion-panels>
+        </v-card-actions>
+        </v-card>
+        <!-- <transition name="fade" appear>
+                <div class="modal-overlay" v-if="showModal"></div>
+            </transition> -->
     </div>
 </template>
 
 <script>
 import axios from "axios"
 import cookies from 'vue-cookies'
+import TweetComments from './TweetComments.vue'
     export default {
         name: 'FeedTweetChild',
+        components: {
+            TweetComments
+        },
         data:() => {
             return {
                 activeUser: "",
@@ -79,6 +107,9 @@ import cookies from 'vue-cookies'
                 tweetLikeCounter: "",
                 toggleLike: false,
                 toggleModal: false,
+                tweetComment: "",
+                newCommentObj: {},
+                tweetCommentInfo: [],
             }
         },
         props: {
@@ -219,6 +250,55 @@ import cookies from 'vue-cookies'
                     console.error("There was an error: " +error);
                 })
             },
+            /* -------------Tweet Comments APIs---------------------*/
+            retrieveTweetComments() {
+                axios.request({
+                    url: 'https://tweeterest.ml/api/comments',
+                    method: 'GET',                                                                                                                                                              
+                    headers: {
+                        'X-Api-Key' : process.env.VUE_APP_API_KEY,
+                    },
+                    params: {
+                        "tweetId" : this.tweetId,
+                    }
+                }).then((response) => {
+                    console.log(response);
+                    this.tweetCommentInfo = response.data;
+    
+                }).catch((error) => {
+                    console.error(error);
+                })
+            },
+            createComment() {
+                axios.request({
+                    url: 'https://tweeterest.ml/api/comments',
+                    method: 'POST',
+                    headers : {
+                        'X-Api-Key' : process.env.VUE_APP_API_KEY,
+                        'Content-Type': 'application/json'
+                    },
+                    data : {
+                        "loginToken" : this.userToken,
+                        "tweetId" : this.tweetId,
+                        "content" : this.tweetComment,
+                    }
+
+                }).then((response) => {
+                    console.log(response);
+                    this.newCommentObj = {
+                        commentId : response.data.commentId,
+                        tweetId : response.data.tweetId,
+                        username : response.data.username,
+                        content : response.data.content,
+                        createdAt : response.data.createdAt,
+                    }
+                    this.tweetCommentInfo.push(this.newCommentObj);
+                    console.log(this.tweetCommentInfo);
+
+                }).catch((error) => {
+                    console.error("There was an error: " +error);
+                })
+            },
 
             getMyCookies() {
                 var getCookie = cookies.get('loginData');
@@ -235,6 +315,9 @@ import cookies from 'vue-cookies'
 </script>
 
 <style lang="scss" scoped>
+    .tweetWrapper {
+        margin-bottom: 3vh;
+    }
     v-btn {
         width: 30%;
     }
@@ -249,6 +332,11 @@ import cookies from 'vue-cookies'
 	padding:9px 23px;
 	text-decoration:none;
 	text-shadow:0px 1px 0px #263666;
+}
+.routerLink {
+    text-decoration: none;
+    color: azure;
+    font-weight: bold;
 }
 .myButton:hover {
 	background:linear-gradient(to bottom, #0688fa 5%, #2dabf9 100%);
@@ -283,5 +371,8 @@ transition: opacity 0.5s;
 }
 .modal {
     display: none;
+}
+#logo {
+    height: 40px;
 }
 </style>
---
+++ src/components/TweetComments.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        {{ username }} | {{ content }} | {{ createdAt }}
+    </div>
+</template>
+
+<script>
+    export default {
+        name : 'TweetComments',
+        props: {
+            commentId : Number,
+            tweetId : Number,
+            username : String,
+            content : String,
+            createdAt : String,
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
