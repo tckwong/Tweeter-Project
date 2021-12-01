e764a746aec4123ea803f452baff645774c75828
Trevor <wtrevor242@gmail.com>
2021-12-01 16:44:08 -0700

    Fixed a base router url

--- src/router/index.js
+++ src/router/index.js
@@ -28,15 +28,11 @@ const routes = [
     name: 'OtherProfilesView',
     component: OtherProfilesView,
   },
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    // component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
 ]
 
 const router = new VueRouter({
   mode: 'history',
-  base: process.env.BASE_URL,
+  base: process.env.VUE_APP_BASE_DOMAIN,
   routes
 })
 
